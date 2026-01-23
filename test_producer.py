e1553d4d81b10b3018719f3083ad488cdb41efa4
bijoy7034 <bijoyanil74@gmail.com>
2026-01-24 00:09:50 +0530

    Refactor test_producer.py by removing commented sections for clarity

--- test_producer.py
+++ test_producer.py
@@ -2,25 +2,24 @@ import pika
 import json
 import os
 
-# RabbitMQ config
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
 RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT", 5672))
 
-# Connect to RabbitMQ
+
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT)
 )
 channel = connection.channel()
 
-# Ensure the queue exists
+
 channel.queue_declare(queue="email.queue", durable=True)
 
-# Test email event
+
 message = {
     "notification_id": "test-001",
     "type": "EMAIL",
     "subject": "Payment",
-    "to": "bijoyanil74@gmail.com",  # Replace with your email
+    "to": "bijoyanil74@gmail.com",  
     "template": "payment_success",
     "data": {
         "name": "Bijoy",
@@ -28,13 +27,12 @@ message = {
     }
 }
 
-# Publish message
 channel.basic_publish(
     exchange="",
     routing_key="email.queue",
     body=json.dumps(message),
     properties=pika.BasicProperties(
-        delivery_mode=2  # Make message persistent
+        delivery_mode=2 
     )
 )
 
